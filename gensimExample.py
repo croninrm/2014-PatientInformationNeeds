7814fd7d44f190a3393cdaabc93e7f52c3b2cb1f
Rob C <croninrm@users.noreply.github.com>
2014-11-14 00:38:19 -0600

    Update gensimExample.py

--- gensimExample.py
+++ gensimExample.py
@@ -33,8 +33,8 @@ from gensim.models import LdaModel
 import os
 import csv
 filetexts = []
-filetexts2 =""
-filesuper=""
+filetextslabeled=[]
+filetexts2 =[]
 os.chdir(inputdirectory)
 
 '''
@@ -46,11 +46,16 @@ Then I can go through each file, and take each document and see its score...
 #107 labeled documents
 # COLUMNS ARE: ID, DATE, MESSAGE, CATEGORY (one or more)
 
-with open('LABELED_2008_175.csv', 'rb') as csvfile:
+for subdir, dirs, files in os.walk(inputdirectory):
+   for file in files:
+       print file
+file='LABELED_2008_175.csv'
+
+with open(file, 'rb') as csvfile:
     spamreader = csv.reader(csvfile)
     filetexts2=[word[3] for word in spamreader]
      
-with open('LABELED_2008_175.csv', 'rb') as csvfile:
+with open(file, 'rb') as csvfile:
     spamreader = csv.reader(csvfile)
     filetextslabeled=[word for word in spamreader]
 
@@ -60,14 +65,22 @@ os.chdir(outputdirectory)
 
 documents=filetexts2
 # remove common words and tokenize
-stoplist = set('for a of the and to in'.split())
+stoplist = set('for a of the and to in i I you my aaabbbccc aaabbbccclicensed or is it on am have me (**place)aaabbbccc would at this detailsaaabbccc. do been can what be just with that was so your will but if had an as -'.split())
 texts = [[word for word in document.lower().split() if word not in stoplist]
          for document in documents]
 
 
 #Create the dictionary from the words in texts, and save it
 dictionary = corpora.Dictionary(texts)
+
+#low_occurence_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 3]
+#dictionary.filter_tokens(low_occurence_ids)
+#dictionary.compactify()
+
+
 dictionary.save('robCSVDictionary.dict') # store the dictionary, for future reference
+
+
 
 #Create the corpus and save it
 corpus = [dictionary.doc2bow(text) for text in texts]
@@ -79,8 +92,17 @@ corpora.MmCorpus.serialize('robCSVcorpus.mm', corpus) # store to disk, for later
 #term freq inverse doc freq
 #trying to ind the frequency on that page versus overall frequency
 
-lda = LdaModel(corpus, num_topics=numTopics)
+lda = LdaModel(corpus, id2word=dictionary,num_topics=numTopics)
 
+ii=0
+
+print 'These are the topics'
+for i in range(0, lda.num_topics):
+    print lda.print_topic(i,topn=20)
+
+
+        
+#sys.exit()
 doc_lda = []
 for i in range(len(corpus)):
     doc_lda.append(lda[corpus[i]])
@@ -126,3 +148,6 @@ with open('CorpusTopics.csv', 'wb') as csvfile:
 
 print "Completed topic modeling"
 sys.exit()
+
+
+
